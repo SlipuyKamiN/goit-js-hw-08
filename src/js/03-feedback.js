bf5e9cb368cb94f312132b284e650301abb163ae
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-02-08 09:46:17 +0100

    corrects mistakes found by mentor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,37 +7,27 @@ const refs = {
 };
 
 const FEEDBACK_DATA = 'feedback-form-state';
-const feedbackData = {};
+let feedbackData = JSON.parse(localStorage.getItem(FEEDBACK_DATA)) || {};
 
-const recoveryData = () => {
-  const dataToRecover = JSON.parse(localStorage.getItem(FEEDBACK_DATA));
-  if (dataToRecover) {
-    refs.email.value = dataToRecover.email;
-    refs.textarea.value = dataToRecover.message;
-  }
+const recoverData = () => {
+  refs.email.value = feedbackData.email || '';
+  refs.textarea.value = feedbackData.message || '';
 };
-recoveryData();
+
+recoverData();
+
 const updateFeedbackData = event => {
   feedbackData[event.target.name] = event.target.value;
   localStorage.setItem(FEEDBACK_DATA, JSON.stringify(feedbackData));
 };
 const onFormSubmitting = event => {
-  // 3-й пункт в ДЗ : "виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями"
-
-  //Варіант 1
-  //Якщо ввести коректні дані, потім оновити сторінку,
-  //і нічого не змінювати в формі, то feedbackData буде порожній об'єкт і законсолиться {}.
-  console.log(feedbackData);
-
-  //Варіант 2
-  //Показує все коректно, але якийсь складний запис, думаю має бути простіший варіант.
-  console.log(JSON.parse(localStorage.getItem(FEEDBACK_DATA)));
-
   event.preventDefault();
 
-  localStorage.removeItem(FEEDBACK_DATA);
+  console.log(feedbackData);
 
+  localStorage.removeItem(FEEDBACK_DATA);
   event.currentTarget.reset();
+  feedbackData = {};
 };
 
 refs.form.addEventListener('input', throttle(updateFeedbackData, 500));
