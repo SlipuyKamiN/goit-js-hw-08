5930604203db7bc38cf0d3bd6ca1cb1304407eb5
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-02-09 15:48:56 +0100

    recoveryTime refactoring

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,15 +4,10 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
 
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
 const updateCurrentTime = data => {
   localStorage.setItem(CURRENT_TIME, data.seconds);
 };
-const recoveryTime = () => {
-  const timeToRecover = localStorage.getItem(CURRENT_TIME);
-  if (timeToRecover) {
-    player.setCurrentTime(timeToRecover);
-  }
-};
 
 player.on('timeupdate', throttle(updateCurrentTime, 1000));
-recoveryTime();
